614e4cade8d846082c2a85d34bf2e2db6066a091
Jen <jen.rodasky@gmail.com>
2020-01-25 20:44:30 -0600

    Create index.php
    Completed Assignment 6

---
+++ assignment06/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Assignment 6</title>
+  <meta charset="utf-8">
+</head>
+<body>
+<!-- Jen Cink
+     index.php
+     INFO2340.WW
+     Theondel
+     1/25/2020
+-->
+
+	<h1>Hello, world!</h1>
+	<div>Allow me to introduce myself! My name is <?php echo"Jen Cink!";?></div>
+	<div>I am <?php echo"32";?>years old and I am a student at Metropolitan Community College.</div>
+	<div>I am currently studying <?php echo"front-end web development ";?>and I hope to graduate in <?php echo"2020!";?></div>
+
+</body>
+</html>
